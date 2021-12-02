7c987396f26c2a0cb488a398392c561677554650
Yususf Khan <yusisamerican@gmail.com>
2021-12-01 18:01:21 -0800

    Taking OpenBSD 7.0 and Deblobbing with LibbertyBSD deblobber

---
+++ usr.bin/ssh/version.h
@@ -0,0 +1,3 @@
+/* $OpenBSD: version.h,v 1.92 2021/09/26 14:01:11 djm Exp $ */
+
+#define SSH_VERSION	"OpenSSH_8.8"
---
+++ usr.sbin/rpki-client/version.h
@@ -0,0 +1,3 @@
+/* $OpenBSD: version.h,v 1.7 2021/11/07 20:57:27 benno Exp $ */
+
+#define RPKI_VERSION	"7.5"
